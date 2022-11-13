bedb571704786ddc20ea3736f4ef9aa053daf21e
Pádua Lima <padualima.dev@gmail.com>
2022-11-13 15:28:51 -0300

    add action upload_data with hotwire turbo frame in index

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,20 @@
 class PortfoliosController < ApplicationController
+
+  # GET /portfolios
   def index
     @portfolios = Portfolio.all
+  end
+
+  # GET /portfolios/upload_data
+  def upload_data
+    @portfolio = Portfolio.new
+
+    select_by_allowed_states
+  end
+
+  private
+
+  def select_by_allowed_states
+    @states = NodeGroup.states_allowed
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :portfolios, only: %i[index] do
+    collection do
+      get 'upload_data'
+    end
+  end
   # Defines the root path route ("/")
   root "portfolios#index"
 end
