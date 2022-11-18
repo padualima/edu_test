212cda4ea9f054794d8677a48572f8697659d93c
Pádua Lima <padualima.dev@gmail.com>
2022-11-17 22:38:26 -0300

    bugfix: filter portfolio by slug and group_year and add position to Portfolio.

---
+++ db/migrate/20221117195317_add_column_position_to_portfolio.rb
@@ -0,0 +1,5 @@
+class AddColumnPositionToPortfolio < ActiveRecord::Migration[7.0]
+  def change
+    add_column :portfolios, :position, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_190858) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_195317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_190858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "expenses_amount_cents"
+    t.integer "position"
     t.index ["member_id"], name: "index_portfolios_on_member_id"
     t.index ["node_group_id"], name: "index_portfolios_on_node_group_id"
   end
